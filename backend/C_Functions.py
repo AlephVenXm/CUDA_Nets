c1d3e54e414a434760475d19c4924be797bcf428
Aleph <168445886+AlephVenXm@users.noreply.github.com>
2024-07-21 15:58:44 +0500

    Create C_Functions.py

---
+++ backend/C_Functions.py
@@ -0,0 +1,58 @@
+import cupy as cu
+
+#Basic operations
+div = cu.ElementwiseKernel(
+    'float64 x, float64 y',
+    'float64 z',
+    'z = x / y',
+    'div'
+)
+mul = cu.ElementwiseKernel(
+    'float64 x, float64 y',
+    'float64 z',
+    'z = x * y',
+    'mul'
+)
+add = cu.ElementwiseKernel(
+    'float64 x, float64 y',
+    'float64 z',
+    'z = x + y',
+    'add'
+)
+sub = cu.ElementwiseKernel(
+    'float64 x, float64 y',
+    'float64 z',
+    'z = x - y',
+    'sub'
+)
+
+#Activations
+gelu = cu.RawKernel(r'''
+const double PI = 3.14159265359;
+extern "C" __global__
+void gelu(const double* x, double* y) {
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    y[i] = 0.5*x[i]*(1+tanh(sqrt(2/PI)*(x[i]+0.044715*pow(x[i], 3))));
+}
+''', 'gelu')
+softmax = cu.RawKernel(r'''                
+const double EXP = 2.71828182845;
+extern "C" __global__
+void softmax(const double* x, double* y) {
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    double numerator[100]; //! This one over here should be pre-configurated
+    double denominator = 0.0;
+    double max = x[blockDim.x*((int)i/blockDim.x)];
+    // Find max
+    for (size_t c = 0; c <= blockDim.x; ++c)
+        if (x[c+blockDim.x*((int)i/blockDim.x)] > max) max = x[c+blockDim.x*((int)i/blockDim.x)];
+    // Find numerator
+    for (size_t c = 0; c < blockDim.x; ++c) {
+        numerator[c] = pow(EXP, x[c+blockDim.x*((int)i/blockDim.x)] - max);
+        denominator += numerator[c];
+    }
+    for (size_t c = 0; c <= blockDim.x; ++c) 
+        numerator[c] /= denominator;
+    y[i] = numerator[threadIdx.x];
+}
+''', 'softmax')
