88d7761071bc694945516677055c456044e3608f
Aleph <168445886+AlephVenXm@users.noreply.github.com>
2024-07-22 17:06:40 +0500

    Update Implementations.py

--- backend/Implementations.py
+++ backend/Implementations.py
@@ -1,73 +1,63 @@
 import cupy as cu
-from C_Functions import add, sub, mul, div, gelu, softmax, normalization
+from C_Functions import *
 
 class Layer:
-    def __init__(self):
+    def __init__(self) -> None:
         self.weights = cu.zeros(shape=(input.shape[1], 1))
         self.biases = cu.zeros(shape=(1,))
-        ...
-    def __call__(self, input):
+    def __call__(self, input) -> cu.ndarray:
         return add(cu.matmul(input, self.weights), self.biases)
 
+class Initializer:
+    class RandomNormal:
+        def __call__(self, inputs, outputs) -> cu.ndarray:
+            return cu.random.normal(size=(inputs, outputs))
+    class Zeros:
+        def __call__(self, inputs) -> cu.ndarray:
+            return cu.zeros(inputs)
+
+class Activation:
+    class GeLU(Layer):
+        def __call__(self, inputs) -> cu.ndarray:
+            outputs = cu.empty(inputs.shape)
+            gelu((inputs.shape[0],), (inputs.shape[1],), (inputs, outputs))
+            return outputs
+    class Softmax(Layer):
+        def __call__(self, inputs) -> cu.ndarray:
+            outputs = cu.empty(inputs.shape)
+            softmax((inputs.shape[0],), (inputs.shape[1],), (inputs, outputs))
+            return outputs
+
 class Dense(Layer):
-    def __init__(self, inputs, outputs, learning_rate=0.1, activation=None):
-        self.learning_rate = learning_rate
-        self.weights = mul(cu.random.randn(inputs, outputs), 10e-3)
-        self.biases = cu.zeros(outputs)
+    def __init__(self,
+                inputs: int, outputs: int,
+                activation=None,
+                kernel_initializer=Initializer.RandomNormal(),
+                bias_initializer=Initializer.Zeros(),
+                use_bias: bool=True) -> None:
+        self.weights = kernel_initializer(inputs, outputs)
+        self.biases = None
         self.activation = activation
-    def __call__(self, input):
+        if use_bias:
+            self.biases = bias_initializer(outputs)
+    def __call__(self, input) -> cu.ndarray:
         if self.activation != None:
             return self.activation(add(cu.matmul(input, self.weights), self.biases))
         return add(cu.matmul(input, self.weights), self.biases)
-    def backward(self, input, gradient_output):
-        gradient_input = cu.dot(gradient_output, cu.transpose(self.weights))
-        gradient_weights = cu.transpose(cu.dot(cu.transpose(gradient_output), input))
-        gradient_biases = cu.sum(gradient_output, axis=0)
-        self.weights = sub(self.weights, mul(self.learning_rate, gradient_weights))
-        self.biases = sub(self.biases, mul(self.learning_rate, gradient_biases))
-        return gradient_input
-
-class GeLU(Layer):
-    def __init__(self):
-        ...
-    def gelu(self, x):
-        res = cu.empty(x.shape)
-        gelu((x[0].shape), (x[1].shape), (x, res))
-        return res
-    def __call__(self, input):
-        return self.gelu(input)
-    def backward(self, input, gradient_output):
-        gelu_gradient = self.gelu(input)
-        return mul(gradient_output, gelu_gradient)
-
-class Softmax(Layer):
-    def __init__(self):
-        ...
-    def softmax(self, x):
-        res = cu.empty(x.shape)
-        softmax((x[0].shape), (x[1].shape), (x, res))
-        return res
-    def __call__(self, input):
-        return self.softmax(input)
-    def backward(self, input, gradient_output):
-        softmax_gradient = self.softmax(input)
-        return mul(gradient_output, softmax_gradient)
 
 class Embedding(Layer):
     ...
 
 class DotProductAttention(Layer):
-    def __init__(self):
-        ...
-    def __call__(self, queries, keys, values, d_k, mask=None):
+    def __call__(self, queries, keys, values, d_k, mask=None) -> cu.ndarray:
         scores = div(cu.matmul(queries, keys), cu.sqrt(d_k))
         if mask is not None:
             scores = add(scores, mul(-1e9, mask))
-        weights = Softmax()(scores)
+        weights = Activation.Softmax()(scores)
         return cu.matmul(weights, values)
 
 class MultiHeadAttention(Layer):
-    def __init__(self, heads, d_k, d_v, d_model):
+    def __init__(self, heads, d_k, d_v, d_model) -> None:
         self.attention = DotProductAttention()
         self.heads = heads
         self.d_k = d_k
@@ -77,7 +67,7 @@ class MultiHeadAttention(Layer):
         self.W_k = Dense(d_k, d_v)
         self.W_v = Dense(d_v, d_model)
         self.W_o = Dense(d_model, d_model)
-    def reshape(self, x, heads, flag=False):
+    def reshape(self, x, heads, flag=False) -> cu.ndarray:
         if flag:
             x = x.reshape(cu.shape(x)[0], cu.shape(x)[1], heads, -1)
             x = x.transpose(0, 2, 1, 3)
@@ -85,7 +75,7 @@ class MultiHeadAttention(Layer):
             x = x.transpose(0, 2, 1, 3)
             x = x.reshape(cu.shape(x)[0], cu.shape(x)[1], self.d_k)
         return x
-    def __call__(self, queries, keys, values, mask=None):
+    def __call__(self, queries, keys, values, mask=None) -> cu.ndarray:
         q_reshaped = self.reshape(self.W_q(queries), self.heads, True)
         k_reshaped = self.reshape(self.W_k(keys), self.heads, True)
         v_reshaped = self.reshape(self.W_v(values), self.heads, True)
@@ -94,17 +84,13 @@ class MultiHeadAttention(Layer):
         return self.W_o(output)
 
 class Normalization(Layer):
-    def __init__(self):
-        ...
-    def __call__(self, input):
-        res = cu.empty(input.shape)
-        normalization((input[0].shape), (input[1].shape), (input, input.mean(), input.var(), res))
-        return res
+    def __call__(self, input) -> cu.ndarray:
+        outputs = cu.empty(inputs.shape)
+        normalization((inputs[0].shape), (inputs[1].shape), (inputs, inputs.mean(), inputs.var(), outputs))
+        return outputs
 
 class Add(Layer):
-    def __init__(self):
-        ...
-    def __call__(self, layers):
+    def __call__(self, layers) -> cu.ndarray:
         sum = 0
         for layer in layers: sum = add(sum, layer)
         return sum
