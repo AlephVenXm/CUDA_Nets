0a0d1435046d01c36a73b96082f2305ef8d810fe
Aleph <168445886+AlephVenXm@users.noreply.github.com>
2024-07-26 21:04:05 +0500

    Update Operation.py

--- backend/Operation.py
+++ backend/Operation.py
@@ -30,7 +30,7 @@ def Add(x, y, thread: int=10, dtype=None) -> cu.ndarray:
 
 def Sub(x, y, thread: int=10, dtype=None) -> cu.ndarray:
     if x.shape != y.shape:
-        print(f"Cannot add arrays of different shapes: Got arrays of shape: {x.shape}, {y.shape}")
+        print(f"Cannot subtract arrays of different shapes: Got arrays of shape: {x.shape}, {y.shape}")
         raise ValueError
     if len(x.shape) > 3 or len(y.shape) > 3:
         print(f"Cannot operate with arrays with rank >= 4: Got arrays of rank: {len(x.shape)}, {len(y.shape)}")
@@ -50,7 +50,7 @@ def Sub(x, y, thread: int=10, dtype=None) -> cu.ndarray:
 
 def Mul(x, y, thread: int=10, dtype=None) -> cu.ndarray:
     if x.shape != y.shape:
-        print(f"Cannot add arrays of different shapes: Got arrays of shape: {x.shape}, {y.shape}")
+        print(f"Cannot multiply arrays of different shapes: Got arrays of shape: {x.shape}, {y.shape}")
         raise ValueError
     if len(x.shape) > 3 or len(y.shape) > 3:
         print(f"Cannot operate with arrays with rank >= 4: Got arrays of rank: {len(x.shape)}, {len(y.shape)}")
@@ -60,17 +60,17 @@ def Mul(x, y, thread: int=10, dtype=None) -> cu.ndarray:
     z = cu.zeros(x.shape, dtype=dtype)
     rank = len(x.shape)
     @cuda.jit(f'void({x.dtype}[{':,'*rank}], {y.dtype}[{':,'*rank}], {z.dtype}[{':,'*rank}])')
-    def sub(x, y, z):
+    def mul(x, y, z):
         idx = cuda.grid(rank)
         z[idx] = x[idx] * y[idx]
     threads = (thread,) * rank
     blocks = tuple([math.ceil(z.shape[i] / threads[i]) for i in range(rank)])
-    sub[blocks, threads](x, y, z)
+    mul[blocks, threads](x, y, z)
     return z
 
 def Div(x, y, thread: int=10, dtype=None) -> cu.ndarray:
     if x.shape != y.shape:
-        print(f"Cannot add arrays of different shapes: Got arrays of shape: {x.shape}, {y.shape}")
+        print(f"Cannot divide arrays of different shapes: Got arrays of shape: {x.shape}, {y.shape}")
         raise ValueError
     if len(x.shape) > 3 or len(y.shape) > 3:
         print(f"Cannot operate with arrays with rank >= 4: Got arrays of rank: {len(x.shape)}, {len(y.shape)}")
@@ -80,17 +80,17 @@ def Div(x, y, thread: int=10, dtype=None) -> cu.ndarray:
     z = cu.zeros(x.shape, dtype=dtype)
     rank = len(x.shape)
     @cuda.jit(f'void({x.dtype}[{':,'*rank}], {y.dtype}[{':,'*rank}], {z.dtype}[{':,'*rank}])')
-    def sub(x, y, z):
+    def div(x, y, z):
         idx = cuda.grid(rank)
         z[idx] = x[idx] / y[idx]
     threads = (thread,) * rank
     blocks = tuple([math.ceil(z.shape[i] / threads[i]) for i in range(rank)])
-    sub[blocks, threads](x, y, z)
+    div[blocks, threads](x, y, z)
     return z
 
 def Linear(k, x, b, thread: int=10, dtype=None) -> cu.ndarray:
     if k.shape != x.shape or k.shape != b.shape or b.shape != x.shape:
-        print(f"Cannot add arrays of different shapes: Got arrays of shape: {k.shape}, {x.shape}, {b.shape}")
+        print(f"Cannot operate with arrays of different shapes: Got arrays of shape: {k.shape}, {x.shape}, {b.shape}")
         raise ValueError
     if len(k.shape) > 3 or len(x.shape) > 3 or len(b.shape) > 3:
         print(f"Cannot operate with arrays with rank >= 4: Got arrays of rank: {len(k.shape)}, {len(x.shape)}, {len(b.shape)}")
@@ -100,10 +100,10 @@ def Linear(k, x, b, thread: int=10, dtype=None) -> cu.ndarray:
     z = cu.zeros(x.shape, dtype=dtype)
     rank = len(x.shape)
     @cuda.jit(f'void({k.dtype}[{':,'*rank}], {x.dtype}[{':,'*rank}], {b.dtype}[{':,'*rank}], {z.dtype}[{':,'*rank}])')
-    def sub(k, x, b, z):
+    def linear(k, x, b, z):
         idx = cuda.grid(rank)
         z[idx] = k[idx] * x[idx] + b[idx]
     threads = (thread,) * rank
     blocks = tuple([math.ceil(z.shape[i] / threads[i]) for i in range(rank)])
-    sub[blocks, threads](k, x, b, z)
+    linear[blocks, threads](k, x, b, z)
     return z
