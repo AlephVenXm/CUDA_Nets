485ea8f17d9be1f92fde1b9fdc7e9d0e33d577d2
Aleph <168445886+AlephVenXm@users.noreply.github.com>
2024-07-29 17:47:22 +0500

    Update Operation.py

--- backend/Operation.py
+++ backend/Operation.py
@@ -129,34 +129,3 @@ def MatDiv(x, y, thread: int=10, dtype=None) -> cu.ndarray:
     blocks = tuple([math.ceil(z.shape[i] / threads[i]) for i in range(rank)])
     matdiv[blocks, threads](PAD(x), PAD(y), z)
     return z
-
-def MatLinear(k, x, b, thread: int=10, dtype=None) -> cu.ndarray:
-    '''
-    Element Wise Linear function: kx + b
-
-    K, X, B -> Matrix or a single Value with rank < 4
-
-    Thread -> number of Threads per one Block. Scales with max rank of Values
-    
-    Dtype -> dtype of Values in Return
-    '''
-    if k.size == 1 and x.size == 1 and b.size == 1:
-        raise ValueError("One of values should be array. It is MATRIX`Linear, not just linear")
-    if (k.shape != x.shape or k.shape != b.shape or b.shape != x.shape) and (k.size > 1 and x.size > 1 and b.size > 1):
-        raise ValueError(f"Cannot operate with arrays of different shapes: Got arrays of shape: {k.shape}, {x.shape}, {b.shape}")
-    if len(k.shape) > 3 or len(x.shape) > 3 or len(b.shape) > 3:
-        raise ValueError(f"Cannot operate with arrays with rank >= 4: Got arrays of rank: {len(k.shape)}, {len(x.shape)}, {len(b.shape)}")
-    if dtype is None:
-        dtype = x.dtype
-    shape = max(k.shape, x.shape, b.shape)
-    rank = len(shape)
-    y = cu.zeros(shape, dtype=dtype)
-    PAD = lambda x : cu.full(shape, x) if x.size == 1 else x
-    @cuda.jit(f'void({k.dtype}[{':,'*rank}], {x.dtype}[{':,'*rank}], {b.dtype}[{':,'*rank}], {y.dtype}[{':,'*rank}])')
-    def linear(k, x, b, y):
-        idx = cuda.grid(rank)
-        y[idx] = k[idx] * x[idx] + b[idx]
-    threads = (thread,) * rank
-    blocks = tuple([math.ceil(y.shape[i] / threads[i]) for i in range(rank)])
-    linear[blocks, threads](PAD(k), PAD(x), PAD(b), y)
-    return y
