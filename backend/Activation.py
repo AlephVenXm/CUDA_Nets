de714a943cd70cc5d24550055be29013a22247cb
Aleph <168445886+AlephVenXm@users.noreply.github.com>
2024-07-29 17:53:26 +0500

    Update Activation.py

--- backend/Activation.py
+++ backend/Activation.py
@@ -1,8 +1,7 @@
 import os
 if os.environ["USE_CUDA"] == "1":
     import cupy as cu
-    if os.environ["USE_C"] == "1":
-        from C_Functions import *
+    from Function import *
 else:
     import numpy as cu
 
@@ -13,10 +12,8 @@ else:
 class ReLU:
     def __call__(self, inputs) -> cu.ndarray:
         self.inputs = inputs
-        if os.environ["USE_CUDA"] == "1" and os.environ["USE_C"] == "1":
-            outputs = cu.empty(inputs.shape)
-            relu((inputs.shape[0],), (inputs.shape[1],), (inputs, outputs))
-            return outputs
+        if os.environ["USE_CUDA"] == "1":
+            return ReLU(inputs)
         return cu.maximum(0, inputs)
     def backward(self, gradient) -> cu.ndarray:
         f = self(self.inputs)
@@ -28,13 +25,11 @@ class ReLU:
             return mul(gradient, (mul(f, (1.0 - f))))
         return gradient * (f * (1.0 - f))
     
-class GeLU:
+class GELU:
     def __call__(self, inputs) -> cu.ndarray:
         self.inputs = inputs
-        if os.environ["USE_CUDA"] == "1" and os.environ["USE_C"] == "1":
-            outputs = cu.empty(inputs.shape)
-            gelu((inputs.shape[0],), (inputs.shape[1],), (inputs, outputs))
-            return outputs
+        if os.environ["USE_CUDA"] == "1":
+            return GELU(inputs)
         return 0.5*inputs*(1+cu.tanh(cu.sqrt(2/cu.pi)*(inputs+0.044715*cu.power(inputs, 3))))
     def backward(self, gradient) -> cu.ndarray:
         inputs = self.inputs
@@ -50,10 +45,8 @@ class GeLU:
 class Softmax:
     def __call__(self, inputs) -> cu.ndarray:
         self.inputs = inputs
-        if os.environ["USE_CUDA"] == "1" and os.environ["USE_C"] == "1":
-            outputs = cu.empty(inputs.shape)
-            softmax((inputs.shape[0],), (inputs.shape[1],), (inputs, outputs))
-            return outputs
+        if os.environ["USE_CUDA"] == "1":
+            return Softmax(inputs)
         numerator = cu.exp(inputs - cu.max(inputs, axis=-1, keepdims=True))
         return numerator / cu.sum(numerator, axis=-1, keepdims=True)
     def backward(self, gradient) -> cu.ndarray:
@@ -69,13 +62,11 @@ class Softmax:
         return grad.reshape(self.inputs.shape) / self.x.shape[0]
     
 class Sigmoid:
-    def __call__(self, inputs, param=1.0) -> cu.ndarray:
+    def __call__(self, inputs) -> cu.ndarray:
         self.inputs = inputs
-        if os.environ["USE_CUDA"] == "1" and os.environ["USE_C"] == "1":
-            outputs = cu.empty(inputs.shape)
-            sigmoid((inputs.shape[0],), (inputs.shape[1],), (inputs, param, outputs))
-            return outputs
-        return 1/(1+cu.power(cu.power(cu.exp(-inputs), param)))
+        if os.environ["USE_CUDA"] == "1":
+            return Sigmoid(inputs)
+        return 1/(1+cu.exp(-inputs))
     def backward(self, gradient) -> cu.ndarray:
         f = self(self.inputs)
         #if os.environ["USE_CUDA"] == "1" and os.environ["USE_C"] == "1":
